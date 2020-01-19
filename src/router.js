9ecc99c8e6dc849047a4c0cec52670a6e094af7f
Nikolas Freitas <nikolasbvf@gmail.com>
2020-01-18 21:13:58 -0300

    :art: Fix lint problems

--- src/router.js
+++ src/router.js
@@ -1,10 +1,10 @@
-import React from "react";
-import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
+import React from 'react';
+import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
 
 export default () => (
   <Router>
     <Switch>
-      <Route path='/'>
+      <Route path="/">
         <p>GG</p>
       </Route>
     </Switch>
